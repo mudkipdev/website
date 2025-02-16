b28f3d20946b015fb72aca838a2c9e8923c0f1e9
mudkipdev <mudkip@mudkip.dev>
2025-02-16 14:19:39 -0700

    add new project

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,11 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
     site: "https://mudkip.dev",
     trailingSlash: "ignore",
-    
+
     devToolbar: {
         enabled: false
     },
-    
+
     markdown: {
         shikiConfig: {
             theme: "catppuccin-macchiato"
