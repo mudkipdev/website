9fb4fa675d7a58ff851f6d7a93b7429fcaf5eae7
mudkipdev <mudkip@mudkip.dev>
2025-02-24 00:15:06 -0700

    switch to ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,19 @@
 import { defineConfig } from "astro/config";
 
+import cloudflare from "@astrojs/cloudflare";
+
 export default defineConfig({
     site: "https://mudkip.dev",
-    trailingSlash: "ignore",
-
     devToolbar: {
         enabled: false
     },
-
     markdown: {
         shikiConfig: {
             theme: "catppuccin-macchiato"
         }
-    }
+    },
+    output: "server",
+    adapter: cloudflare({
+        imageService: "passthrough"
+    })
 })
