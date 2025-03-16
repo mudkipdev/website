8f997c1b72e60fec0599717ffa71cdf557c3bd5a
mudkipdev <mudkip@mudkip.dev>
2025-03-16 05:34:51 -0600

    fix cloudflare

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,8 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+type Runtime = import("@astrojs/cloudflare").Runtime<ENV>;
+
+declare namespace App {
+	interface Locals extends Runtime {}
+}
